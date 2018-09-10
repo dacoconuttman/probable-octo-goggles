c0154c59fd914e30939c4c7cc181ee8f8554e69b
Andrew Ksendzoff <dacoconuttman@gmail.com>
2018-09-10 18:38:23 -0500

    initial commit

---
+++ lib/probable_octo_goggles.ex
@@ -0,0 +1,18 @@
+defmodule ProbableOctoGoggles do
+  @moduledoc """
+  Documentation for ProbableOctoGoggles.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ProbableOctoGoggles.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/probable_octo_goggles_test.exs
@@ -0,0 +1,8 @@
+defmodule ProbableOctoGogglesTest do
+  use ExUnit.Case
+  doctest ProbableOctoGoggles
+
+  test "greets the world" do
+    assert ProbableOctoGoggles.hello() == :world
+  end
+end
